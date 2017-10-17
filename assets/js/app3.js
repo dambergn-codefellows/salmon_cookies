7cce2b3271718dbd93cbd3abdf3f4773d92c3933
dambergn <dambergn@gmail.com>
2017-10-17 15:24:51 -0700

    added app3.js for testing.

--- assets/js/app3.js
+++ assets/js/app3.js
@@ -8,7 +8,8 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 //we need to create a place for our list of all locations to be placed when we create them.
 var allLocations = [];
-//console.log(allLocations);
+console.log(allLocations);
+
 //we need to define a varaiable that will be dynamic depending on how many cookies
 var totalCookiesByHour = [];
 var totalCookiesByLocation = [];
@@ -22,7 +23,7 @@ var capitolHillData = new MakeLocation('Capitol Hill', 20, 38, 2.3);
 var alkiData = new MakeLocation('Alki', 2, 16, 4.6);
 
 //constructor functions begin with an Uppercase letter
-function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour){
+function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour, randCustByHour, cookiesSoldByHour){
   this.name = name;
   this.minCustPerHour = minCustPerHour;
   this.maxCustPerHour = maxCustPerHour;
@@ -31,11 +32,11 @@ function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour
   this.randCustByHour = [];
   //console.log(this.randCustByHour);
   this.cookiesSoldByHour = [];
-  console.log(this.cookiesSoldByHour);
-  this.totalCookiesByLocation = [];
+  //console.log(this.cookiesSoldByHour);
+  this.totalCookiesByLocation = []; //works
   console.log(this.totalCookiesByLocation);
   this.totalCookiesByHour = [];
-  console.log(this.totalCookiesByHour);
+  //console.log(this.totalCookiesByHour);
   allLocations.push(this.name);
   for(var i = 0; i < hours.length; i++) {
     this.randCustByHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
@@ -54,7 +55,7 @@ function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour
   this.totalCookiesByLocation.push(sumByLocation);
   //console.log('Total: ' + sum);
   var sumByHour = 0;
-  for(var m = 0; l < allLocations.length; m++) {
+  for(var m = 0; m < allLocations.length; m++) {
     sumByHour += (this.randCustByHour[m] + this.cookiesSoldByHour[m]);
   }
   this.totalCookiesByHour.push(sumByHour);
@@ -79,15 +80,17 @@ makeHeaderRow();
 
 function makeFirstAndPikeRows() {
   var cookiestands = document.getElementById('cookiestands');
-  var trEl = document.createElement('tr'); //creates table div.
-  //console.log('Before trEl', trEl);
-  trEl.textContent = firstAndPikeData.name;
-  for(var i = 0; i < hours.length; i++){
-    var tdEl = document.createElement('td'); //creates top table row.
-    tdEl.textContent = firstAndPikeData.cookiesSoldByHour[i];
-    trEl.appendChild(tdEl);
+  for(var j = 0; i < allLocations.length; j++){
+    var trEl = document.createElement('tr'); //creates table div.
+    //console.log('Before trEl', trEl);
+    trEl.textContent = firstAndPikeData.name;
+    for(var i = 0; i < hours.length; i++){
+      var tdEl = document.createElement('td'); //creates top table row.
+      tdEl.textContent = firstAndPikeData.cookiesSoldByHour[i];
+      trEl.appendChild(tdEl);
+    }
+    cookiestands.appendChild(trEl);
   }
-  cookiestands.appendChild(trEl);
 }
 makeFirstAndPikeRows();
 
@@ -152,7 +155,7 @@ function makeTotalsRows() {
   var trEl = document.createElement('tr'); //creates table div.
   //console.log('Before trEl', trEl);
   trEl.textContent = 'Total';
-  for(var i = 0; i < allLocations.length; i++){
+  for(var i = 0; i < allLocations.length[0]; i++){
     var tdEl = document.createElement('td'); //creates top table row.
     tdEl.textContent = alkiData.cookiesSoldByHour[i];
     trEl.appendChild(tdEl);
