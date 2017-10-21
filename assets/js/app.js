7824b89779e86f5dc9802432ba9671c8e6b72ea9
dambergn <dambergn@gmail.com>
2017-10-21 13:58:00 -0700

    finished most CSS except for index page.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,10 @@
 'use strict';
-//constructors! think of it like a specialized machine in a factory that creates new objects.
-//prototypes are the functionality that the machine adds to the objects
-
-//we need to define our hours and store them
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm',];
-
-//we need to create a place for our list of all locations to be placed when we create them.
 var allLocations = [];
-//console.log(allLocations);
-//we need to define a varaiable that will be dynamic depending on how many cookies
 var cookiesSoldByHour = [];
 console.log(cookiesSoldByHour);
 var cookiesByHourTotal = [];
 console.log(cookiesByHourTotal);
-//total of total cookies
-var netTotal = 0;
 makeHeaderRow();
 new MakeLocation('First and Pike', 23, 65, 6.3);
 new MakeLocation('SeaTack Airport', 3, 24, 1.2);
@@ -23,7 +13,6 @@ new MakeLocation('Capitol Hill', 20, 38, 2.3);
 new MakeLocation('Alki', 2, 16, 4.6);
 makeTotalsRow();
 
-//constructor functions begin with an Uppercase letter
 function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour){
   var sumByLocation = 0;
   var sumByHour = 0;
@@ -33,7 +22,6 @@ function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour
   this.avgCookieSoldPerHour = avgCookieSoldPerHour;
   this.randCustByHour = [];
   this.cookiesSoldByHour = [];
-  //console.log(this.cookiesSoldByHour);
   this.totalCookiesByLocation = [];
   this.totalCookiesByHour = [];
   allLocations.push(this.name);
@@ -41,7 +29,6 @@ function MakeLocation(name, minCustPerHour, maxCustPerHour, avgCookieSoldPerHour
   for(var i = 0; i < hours.length; i++) {
     this.randCustByHour.push(Math.floor(Math.random() * (this.maxCustPerHour - this.minCustPerHour + 1)) + this.minCustPerHour);
   }
-//method for cookies sold by hours
   for(var j = 0; j < hours.length; j++) {
     this.cookiesSoldByHour.push(Math.round(this.avgCookieSoldPerHour * this.randCustByHour[j]));
     sumByLocation += (this.cookiesSoldByHour[j]);//metohd for Calculating Total
---
+++ assets/js/hide.js
@@ -0,0 +1,44 @@
+function showHide1() {
+  var x = document.getElementById("firstandpikemap");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showHide2() {
+  var x = document.getElementById("seatacairmap");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showHide3() {
+  var x = document.getElementById("seatcentmap");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showHide4() {
+  var x = document.getElementById("caphilmap");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function showHide5() {
+  var x = document.getElementById("alkimap");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
